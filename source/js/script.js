7d4407a7ccbfc78b73b1d0738cc14af3afa46972
Wh_Frost <whfrost63@gmail.com>
2020-04-24 00:05:38 +0400

    Начало стилизации адаптивности

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,42 @@
+var myMap = document.getElementById("map__canvas");
+
+if (myMap) {
+
+  ymaps.ready(function () {
+    myMap = new ymaps.Map('map__canvas', {
+      center: [59.938655, 30.323143],
+      zoom: 17
+    }, {
+        searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'ул. Большая Конюшенная, 19/8',
+        balloonContent: 'ул. Большая Конюшенная, 19/8'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: './img/icon-map-pin.svg',
+          // Размеры метки.
+          iconImageSize: [67, 100],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-40, -140]
+        }),
+
+    myMap.geoObjects
+      .add(myPlacemark)
+  });
+}
+
+
 var navMain = document.querySelector('.main-menu');
 var navToggle = document.querySelector('.main-menu__toggle');
 
